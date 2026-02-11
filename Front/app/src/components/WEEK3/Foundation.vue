339574f430ae7113ce25211427fddac1b952ab48
M_Ibrokhiiim <ibrokhimmuysinov@gmail.com>
2026-02-11 19:20:43 +0500

    Project skeleton structured

---
+++ Front/app/src/components/WEEK3/Foundation.vue
@@ -0,0 +1,19 @@
+<template>
+    <TAG :tag ="tag"/>
+  <div class="border-2  border-blue-200  rounded-xl h-[550px]">
+    <h1 class="font-serif">TO-DO</h1>
+    <input/>
+    <span></span>
+    <div>
+     <ol class="list-decimal list-inside">
+        <li class="ml-0">Run away</li>
+        <li>Run away</li>
+     </ol>
+    </div>
+  </div>
+</template>
+<script setup lang="ts">
+import { ref } from 'vue'
+import TAG from '../WEEK-TAG/TAG.vue'
+const tag = ref<string>('week3')
+</script>
--- Front/app/src/components/Week2-Api-Wrapper.vue
+++ Front/app/src/components/Week2-Api-Wrapper.vue
@@ -1,17 +1,19 @@
 <script setup  lang="ts">
-import { onMounted } from 'vue';
-import { useProducts } from '../composables/useProductsAPI';
+import { onMounted,ref } from 'vue'
+import { useProducts } from '../composables/useProductsAPI'
+
+import TAG from './WEEK-TAG/TAG.vue'
 
 const {loading, getProducts, data } = useProducts()
+const tag = ref<string>('week2')
 
 onMounted(()=>{
   getProducts()
-  // console.log(data.value)
 })
  </script>
 <template>
   <div>
-    <h1 class="uppercase text-center rounded-[40px]  border-[5px]  mb-[30px] border-black w-[15vw]  bg-blue-100">Week2</h1>
+    <TAG :tag="tag"/>
      <h1 class="mb-10" v-if="loading">API wrapper is working here ...</h1>
      <h1 v-else v-for="product in data" :key="product.id">
       <a :href="product.path">{{ product.name.RU }}</a>
