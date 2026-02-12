449b340a090cfb64c23712a427e8136c17d454a2
M_Ibrokhiiim <ibrokhimmuysinov@gmail.com>
2026-02-12 20:20:04 +0500

    Task fully finished with composables

---
+++ Front/app/src/components/WEEK3/Input-Part.vue
@@ -0,0 +1,24 @@
+<template>
+     <h1 class="font-bold font-serif text-[35px] text-blue-400 mb-[30px] mt-[20px]">TO-DO</h1>
+    <div class="flex justify-center items-center text-center  ml-[20px ]">
+      <input 
+      autofocus
+      v-model="input"
+      placeholder = "Type..."
+      class="border-2 p-2 w-[350px] h-[40px] outline-none border-blue-400 rounded-md text-[20px] pl-[10px]"
+      /> 
+      <button @click="addTask(input)" class="bg-blue-400 ml-2 h-[40px] w-[100px]   
+                    text-center text-[25px] font-semibold 
+                    flex outline-none active:outline-none  
+                    transition-all  active:border-none active:scale-75 
+                    active:duration-100  border-none text-white
+                    items-center  focus:outline-none">
+        Add
+      </button>
+    </div>
+</template>
+<script setup lang="ts" >
+import useTODO  from '../../composables/useTodoActions'
+
+const { input,addTask } = useTODO()
+</script>
---
+++ Front/app/src/components/WEEK3/Lists-Part.vue
@@ -0,0 +1,21 @@
+<template>
+ <p class="h-[1.5px] w-[450px] mt-[40px] bg-blue-200"></p>
+  <div class="mt-[20px]  h-[40vh]  overflow-scroll">
+     <ol class=" mt-[10px]" v-for="(item,index) in listTasks" :key="item.id">
+        <div class="flex w-[460px] items-center justify-between">
+          <li class="text-[20px]">{{index+1}}. {{ item.name }}</li>
+          <button  @click="removeTask(item.id)"  class="w-[10px] focus:outline-none    border-none outline-none bg-blue-400 transition-all duration-75 active:scale-50 h-[25px]  flex items-center  justify-center text-center">
+            <p class="text-xl text-white mt-[-2px]"">x</p>
+          </button>
+        </div>
+     </ol>
+    </div>
+</template>
+<script setup lang="ts">
+import useTODO from '../../composables/useTodoActions';
+
+const {listTasks, removeTask} = useTODO()
+
+
+   
+</script>
