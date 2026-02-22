860ecda4e5aa1d8b222a121c98444bae8f5faa08
M_Ibrokhiiim <ibrokhimmuysinov@gmail.com>
2026-02-22 13:23:02 +0500

    Routes splitted into seperate files

--- Front/app/src/router/index.ts
+++ Front/app/src/router/index.ts
@@ -1,16 +1,14 @@
 import { createRouter, createWebHistory } from 'vue-router';
 import { Week4HeaderRoutes } from './week4.header.routes';
 import { Week3HeaderRoutes } from './week3.header.routes';
-
-const routes = [
-  ...Week3HeaderRoutes,
-  ...Week4HeaderRoutes
-]
-
+ 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
-  routes 
-});
+  routes: [
+     ...Week3HeaderRoutes,
+     ...Week4HeaderRoutes
+  ]
+})
 
  
 
