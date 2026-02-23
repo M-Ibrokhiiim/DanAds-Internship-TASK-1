07cdcd3079a7d7f992b84bb664b2bc968e89ce06
M_Ibrokhiiim <ibrokhimmuysinov@gmail.com>
2026-02-23 14:52:01 +0500

    BaseLayout made small changes

--- Front/app/src/components/week5/form/BaseLayout.vue
+++ Front/app/src/components/week5/form/BaseLayout.vue
@@ -1,21 +1,22 @@
 <template>
-    <div class="relative ">
+    <div class="relative  mb-[70px] mt-[70px]">
         <form action="submit" class=" gap-1 items-center  flex justify-center w-[500px] ml-[3vw]">
          <div class=" w-[70%]  border-2 border-blue-300 rounded-3xl shadow-2xl flex flex-col justify-center items-center h-[250px]">
             <main class="flex flex-col  items-center gap-1 w-[500px] mt-[60px]">
                <div class=" w-[51%] flex  justify-between" >
                 <label for="name">Name:</label>
                 <input 
+                  autofocus
                   type="text"
-                  class="border border-black rounded-[3px] focus:outline-none pl-[2px]"
+                  class="border rounded-[3px] focus:outline-none pl-[2px]"
                   placeholder="John"
                   />
             </div>
-            <div class="  w-[51%] flex justify-between" >
+            <div class=" w-[51%] flex justify-between" >
                 <label for="name">Surname:</label>
                 <input 
                   type="text"
-                  class="border border-black rounded-[3px] focus:outline-none pl-[2px]"
+                  class="border rounded-[3px] focus:outline-none pl-[2px]"
                   placeholder="Roll"
                   />
             </div>
@@ -23,13 +24,13 @@
                 <label for="name">Age:</label>
                 <input 
                   type="number"
-                  class="border border-black rounded-[3px] focus:outline-none pl-[2px]"
+                  class="border-[1px]  rounded-[3px] focus:outline-none pl-[2px]"
                   min="0"
                   placeholder="0"
                   />
             </div>
             </main>
-            <button type="submit" @click.prevent="" class="ml-[180px] mt-4 focus:outline-none bg-blue-300 transition-all duration-100 border-none active:scale-75  text-white">Send</button>
+            <button type="submit" @click.prevent="" class="ml-[180px] mt-4 focus:outline-none bg-blue-400 opacity-[0.8] transition-all duration-100 border-none active:scale-75  text-white">Send</button>
          </div>
         </form>
         <div @click="$emit('isOpenModal', true)" class="absolute top-[20px] right-[140px] cursor-pointer transition-all duration-100 active:scale-50 ">
@@ -40,7 +41,5 @@
 <script setup lang="ts">
 import List from '@/icons/List.vue'
 const emit = defineEmits(['isOpenModal'])
-
-
 
 </script>
