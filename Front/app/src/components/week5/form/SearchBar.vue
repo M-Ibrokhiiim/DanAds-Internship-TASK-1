f003fc581e257d722220f9b2cdda50074af0aacd
M_Ibrokhiiim <ibrokhimmuysinov@gmail.com>
2026-02-23 13:52:38 +0500

    UserList modal implemented

--- Front/app/src/components/week5/form/SearchBar.vue
+++ Front/app/src/components/week5/form/SearchBar.vue
@@ -1,16 +1,16 @@
 <template>
-    <div class="absolute top-[80px] w-[100%]  flex justify-center">
-                 <div class="flex h-[40px] justify-between items-center   w-[80%]">
+    <div class="absolute top-[60px] w-[100%]  flex justify-center">
+                <div class="flex h-[90px] justify-between items-center   w-[80%]">
                     <input 
                             type="text" 
                             placeholder="Search..." 
-                            class="flex-grow px-4 py-2 text-gray-700 h-[40px]   border-r-0 focus:outline-none border-[2px] border-blue-400 shadow-xl rounded-l-full "
+                            class="flex-grow px-4 py-2 text-gray-700 h-[44px]   border-r-0 focus:outline-none border-[2px] border-blue-400 shadow-xl rounded-l-full "
                      />
                     <button 
-                        class="bg-blue-400 hover:bg-blue-500 active:bg-blue-400 focus:outline-none border-none text-white px-4 py-2 rounded-r-full transition"
+                        class="bg-blue-400 hover:bg-blue-500 h-[45px] active:bg-blue-400 focus:outline-none border-none text-white px-4 py-2 rounded-r-full transition"
                         >
                         Search
                     </button>
-                 </div>
+                </div>
     </div>
 </template>
